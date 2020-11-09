51cc901ea5e559a7f14102c8961f849d699215d5
Nikita Gudkov <gudkovwork@gmail.com>
2020-11-09 15:55:47 +0100

    - small fix

--- order_service/src/main/java/com/example/messaging/OrderJmsProducer.java
+++ order_service/src/main/java/com/example/messaging/OrderJmsProducer.java
@@ -3,9 +3,7 @@ package com.example.messaging;
 import com.example.dto.messaging.OrderUpdatedEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.stereotype.Component;
 
-@Component
 public class OrderJmsProducer {
 
     private static final Logger LOGGER = LogManager.getLogger(OrderJmsProducer.class);
